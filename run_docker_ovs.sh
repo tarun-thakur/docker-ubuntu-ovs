35a34eabf2e229ea8cc8d7bb5d97d7acbc6a35ba
tarun-thakur <40660954+tarun-thakur@users.noreply.github.com>
2018-07-11 14:05:52 +0530

    Update run_docker_ovs.sh

--- run_docker_ovs.sh
+++ run_docker_ovs.sh
@@ -10,15 +10,8 @@ echo "Controller IP: " $2
 
 for ((i=1;i<=$1;i++)); do
     # NOTE: Change your docker image name in below line while using this script
-    docker run -itd --name ovs$i -e MODE=tcp:$2 --cap-add NET_ADMIN docker-ubuntu-ovs-img
-    # Get IP address of container;
-    # NOTE: docker networking by default allocates ip-address of subnet 172.17.0.0/16 that is why "grep 172" in search
-    local_ip=$(docker exec ovs$i ip a show | awk '/inet / {print $2}' | grep -o ^[^/]* | grep 172)
+    docker run -itd --rm --name ovs$i -e MODE=tcp:$2 --cap-add NET_ADMIN docker-ubuntu-ovs-img
     sleep 2
-
-    # set local_ip used as TEP IP address if ovs will play role in tunnel
-    docker exec ovs$i ovs-vsctl set O . other_config:local_ip=$local_ip
     echo "Started docker container ovs$i."
-    sleep 1
 done
 
