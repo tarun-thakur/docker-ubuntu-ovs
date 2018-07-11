bd49c681b3a94d24519e3d2b2521e2df2fb03edd
tarun-thakur <40660954+tarun-thakur@users.noreply.github.com>
2018-07-11 14:04:24 +0530

    Update configure-ovs.sh

--- configure-ovs.sh
+++ configure-ovs.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 ovs_version=$(ovs-vsctl -V | grep ovs-vsctl | awk '{print $4}')
 ovs_db_version=$(ovsdb-tool schema-version /usr/share/openvswitch/vswitch.ovsschema)
-local_ip=$(ip a show enp0s8 | awk '/inet / {print $2}' | grep -o ^[^/]*)
+# Get IP address of container;
+# NOTE: docker networking by default allocates ip-address of subnet 172.17.0.0/16 that is why "grep 172" in search
+local_ip=$(ip a show | awk '/inet / {print $2}' | grep -o ^[^/]* | grep 172)
 mode=${1:-tcp}
 
 # give ovsdb-server and vswitchd some space...
@@ -13,6 +15,7 @@ ovs-vsctl --no-wait -- set Open_vSwitch . ovs-version="${ovs_version}"
 ovs-vsctl --no-wait -- set Open_vSwitch . system-type="docker-ovs"
 ovs-vsctl --no-wait -- set Open_vSwitch . system-version="0.1"
 ovs-vsctl --no-wait -- set Open_vSwitch . external-ids:system-id=`cat /proc/sys/kernel/random/uuid`
+# set local_ip used as TEP IP address if ovs running under docker container will play role in tunnel
 ovs-vsctl set Open_vSwitch . other_config:local_ip=$local_ip
 ovs-vsctl --no-wait -- add-br br-int
 if [ $mode == "ptcp" ]
